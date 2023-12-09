53e363186170f274b534677dcb0b87c333fba3a1
Ivan <daniloffivan0@gmail.com>
2023-12-09 18:17:34 +0500

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
+);
+server.use(router);
+
+server.listen(8000, () => {
+  console.log("JSON SERVER is running");
+});
+
+module.exports = server;
